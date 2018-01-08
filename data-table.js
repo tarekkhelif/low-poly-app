721538aac392e8e2d113486d24145deedd3bd48f
Tarek Khelif <tokhelif@gmail.com>
2018-01-07 18:02:22 -0800

    Use drag/dropped .csv file for table
    Before, I chose the `.csv` file for the table using the HTML file
    selection dialog.  So far the only way I've figured out how to read
    a file in PaperScript is by dragging and dropping a file into the
    document, so for now I'll populate the data table with the same
    drag-and-dropped file and remove the ability to chose a file with the
    HTML file selection dialog.
    
    Also, I renamed progress.js to data-table.js.

---
+++ data-table.js
@@ -0,0 +1,34 @@
+// Read and display data
+document.addEventListener('drop', (e) => {
+    e.preventDefault();
+
+    // Get reference
+    const table = document.getElementById('table');
+    
+    for (const f of event.dataTransfer.files) { // File is in a 1-element list of files
+        // PapaParse
+        Papa.parse(f, {
+            error: (err, file) => {
+                console.log("ERROR:", err, file);
+            },
+
+            complete: (results) => {
+                console.log("Done with all data");
+                
+                // Display data as table
+                for (const row of results.data) {
+                    const tr = document.createElement('tr');
+                    for (const crd of row) {
+                        const td = document.createElement('td');
+                        td.innerText = Math.round(crd);
+                        tr.appendChild(td);
+                    }
+                    table.appendChild(tr);
+                }
+
+            },
+
+            dynamicTyping: true // Recognize as numbers
+        });
+    }
+})
