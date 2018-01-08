5ae355e51b0f52a9e4bcdc9418516fbd9ce895d4
Tarek Khelif <tokhelif@gmail.com>
2018-01-07 19:04:26 -0800

    Do plotting and table-filling in same file
    Consolidated `draw-path.js` and `data-table.js` into one file, renamed
    to `progress.js`.
    
    The drawback to doing this is that I'm doing everything in PaperScript,
    which is based on ES5, so it doesn't have the nice new features that I
    like to use.

--- progress.js
+++ progress.js
@@ -11,20 +11,32 @@ function onDocumentDrop(e) {
     event.preventDefault();
 
     var f = event.dataTransfer.files[0];
+    
+    // Get reference
+    var table = document.getElementById('table');
         
     // PapaParse
     Papa.parse(f, {
         dynamicTyping: true, // Recognize as numbers
-        skipEmptyLines: true, // My `.csv` files end in an empty line
+        skipEmptyLines: true, // My `.csv` files end in an empty line, which screws some things up
 
         error: function (err, file) {
             console.log("ERROR:", err, file);
         },
 
         complete: function (results) {
-            console.log("Plotting CSV data");
+            // Display data as table
+            results.data.forEach(function (row) {
+                var tr = document.createElement('tr');
+                row.forEach(function (crd) {
+                    var td = document.createElement('td');
+                    td.innerText = Math.round(crd);
+                    tr.appendChild(td);
+                });
+                table.appendChild(tr);
+            });
 
-            // Display data in canvas
+            // Plot data in canvas
             results.data.forEach(function (row, i) {
                 var x = row[0];
                 var y = 600 - row[1]; // Looks like it's necessary to invert y??
