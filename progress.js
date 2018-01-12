1d9c6fd1f8c666bed21992a2c2e554dcfea559d2
Tarek Khelif <tokhelif@gmail.com>
2018-01-11 19:32:34 -0800

    Incorporate Prettier and run it

--- progress.js
+++ progress.js
@@ -20,39 +20,41 @@ function onDocumentDrop(event) {
     event.preventDefault();
 
     var f = event.dataTransfer.files[0];
-    
+
     // Get reference
-    var table = document.getElementById('table');
-        
+    var table = document.getElementById("table");
+
     // PapaParse
     Papa.parse(f, {
         dynamicTyping: true, // Recognize as numbers
         skipEmptyLines: true, // My `.csv` files end in an empty line, which screws some things up
 
-        error: function (err, file) {
+        error: function(err, file) {
             console.log("ERROR:", err, file);
         },
 
-        complete: function (results) {
+        complete: function(results) {
             numberOfDataSets++;
-            
+
             // Turn data into array of paperjs `Points`
             var points = [];
-            results.data.forEach(function (row) {
+            results.data.forEach(function(row) {
                 points.push(new Point(row));
             });
 
             // Find x_max and y_max
             function slice2D(col, arr) {
-                return arr.map(function (row) {return row[col];});
+                return arr.map(function(row) {
+                    return row[col];
+                });
             }
             var xs = slice2D(0, results.data);
             var ys = slice2D(1, results.data);
             var x_max = Math.max.apply(null, xs);
             var y_max = Math.max.apply(null, ys);
-            
+
             // Resize canvas to fit data
-            view.viewSize.width  = Math.max(x_max, view.viewSize.width);
+            view.viewSize.width = Math.max(x_max, view.viewSize.width);
             view.viewSize.height = Math.max(y_max, view.viewSize.height);
             view.center = new Point(view.size.width / 2, view.size.height / 2);
 
@@ -62,7 +64,7 @@ function onDocumentDrop(event) {
                 yt: 0,
                 yb: view.bounds.height
             };
-            
+
             // Plot first data set as path
             if (numberOfDataSets !== 1) {
                 var outline = new Path({
@@ -74,18 +76,17 @@ function onDocumentDrop(event) {
                 var box = Path.Rectangle(outline.bounds);
                 console.log(box);
                 var cover = box.subtract(outline);
-                cover.fillColor = 'white';
-            }
-            else {
+                cover.fillColor = "white";
+            } else {
                 sites = points;
                 renderDiagram();
             }
 
-            points.forEach(function (point) {
+            points.forEach(function(point) {
                 // Display data as table
-                var tr = document.createElement('tr');
-                var tdx = document.createElement('td');
-                var tdy = document.createElement('td');
+                var tr = document.createElement("tr");
+                var tdx = document.createElement("td");
+                var tdy = document.createElement("td");
                 tdx.innerText = Math.round(point.x);
                 tdy.innerText = Math.round(point.y);
                 tr.appendChild(tdx);
@@ -100,13 +101,12 @@ function onDocumentDrop(event) {
                     opacity: 1
                 }); */
             });
-            
+
             // Actually update the canvas
             view.update();
         }
     });
 }
-
 
 function renderDiagram() {
     var diagram = voronoi.compute(sites, bbox);
@@ -137,7 +137,7 @@ function createPath(points) {
 
     for (var i = 0, l = points.length; i < l; i++) {
         var point = points[i];
-        var next = points[(i + 1) == points.length ? 0 : i + 1];
+        var next = points[i + 1 == points.length ? 0 : i + 1];
         var vector = (next - point) / 2;
         path.add({
             point: point + vector,
@@ -152,7 +152,7 @@ function createPath(points) {
 
 function removeSmallBits(path) {
     var min = path.length / 50;
-    for(var i = path.segments.length - 1; i >= 0; i--) {
+    for (var i = path.segments.length - 1; i >= 0; i--) {
         var segment = path.segments[i];
         var cur = segment.point;
         var nextSegment = segment.next;
@@ -163,6 +163,6 @@ function removeSmallBits(path) {
     }
 }
 
-document.addEventListener('drop', onDocumentDrop, false);
-document.addEventListener('dragover', onDocumentDrag, false);
-document.addEventListener('dragleave', onDocumentDrag, false);
+document.addEventListener("drop", onDocumentDrop, false);
+document.addEventListener("dragover", onDocumentDrag, false);
+document.addEventListener("dragleave", onDocumentDrag, false);
