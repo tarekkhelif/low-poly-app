eab2bda00b0120da3ff65aa5444fa1a55c277d0a
Tarek Khelif <tokhelif@gmail.com>
2018-01-25 18:52:15 -0800

    Make mona example work

--- src/iswtw.js
+++ src/iswtw.js
@@ -1,12 +1,31 @@
+const Paper = paper;
+
 // Confirm that the "imports" are working
-console.log(paper); // import paper as Paper from "paper";
+console.log(Paper); // import paper as Paper from "paper";
 console.log(Papa); // import Papa from "papaparse";
 console.log(Voronoi); // import Voronoi from "voronoi";
 
-paper.install(window);
-window.onload = () => {
-    paper.setup("canvas");
-    const rect = new paper.Path.Rectangle([75, 75], [100, 100]);
-    rect.strokeColor = "black";
-    paper.view.onFrame = () => rect.rotate(3);
+// Create canvas and associate it with `Paper`
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+document.body.appendChild(canvas);
+Paper.setup(canvas);
+
+// Create a raster item using the image tag with id='mona'
+const raster = new Paper.Raster("mona.jpg");
+
+// Make canvas/view same size as raster
+Paper.view.viewSize = raster.size;
+
+// Move the raster to the center of the Paper.view
+raster.position = Paper.view.center;
+
+// Draw circle at mouse position filled with the
+// average color of the raster under the circle
+raster.onMouseMove = (event) => {
+    const region = new Paper.Path.Circle({
+        center: event.point,
+        radius: 15,
+    });
+    region.fillColor = raster.getAverageColor(region);
 };
