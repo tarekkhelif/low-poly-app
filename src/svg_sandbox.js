d3d8b49a9f80ed8b6187727e11e258991146e784
Tarek Khelif <tokhelif@gmail.com>
2018-01-30 13:16:22 -0800

    Draw dots on click, change color on second click

--- src/svg_sandbox.js
+++ src/svg_sandbox.js
@@ -8,10 +8,25 @@ console.log(SVG);
 // Use svg.js within the element with id `#drawing`
 const drawing = SVG("drawing").size(300, 300);
 
-// Draw some things
-const rect = drawing.rect(100, 100).attr({ fill: "#dd6" });
-drawing
-    .rect(100, 100)
-    .animate()
-    .fill("#f03")
-    .move(100, 100);
+// // Draw some things
+// const rect = drawing.rect(100, 100).attr({ fill: "#dd6" });
+// drawing
+//     .rect(100, 100)
+//     .animate()
+//     .fill("#f03")
+//     .move(100, 100);
+
+const bkgd = drawing.rect(300, 300).fill("#00000000");
+bkgd.on("mousedown", placeDot);
+
+function placeDot(e) {
+    const marker = drawing
+        .circle(10)
+        .fill("#69f")
+        .stroke("#339")
+        .center(e.offsetX, e.offsetY);
+
+    marker.on("mousedown", (e) => {
+        marker.fill("#c22").stroke("#f00");
+    });
+}
