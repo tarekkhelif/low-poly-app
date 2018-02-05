39c91cfa25cc9ed655ff80702fe1cf96ec662f9b
Tarek Khelif <tokhelif@gmail.com>
2018-02-04 18:31:20 -0800

    Plan what I want the final version of the app to look like

---
+++ app_plan.js
@@ -0,0 +1,29 @@
+// import d3 from "D3";
+// import paper from "paper";
+/* global d3: false, paper: false */
+
+// Set up  blank canvas
+
+// UI: Load raster
+
+// UI: Outline raster
+
+// Generate sites for initial Voronoi tesselation
+
+// UI: move/add/delete sites
+
+// Perform Voronoi tesselation and color based on raster
+
+// UI: Edit tesselation
+/* TODO
+ * - move/add/delete vertices
+ * - add/delete links
+ * - default to recalculating color
+ * - optionally lock color so it isn't recalculated
+ * - choose custom color
+ */
+
+// UI: Download SVG to local machine
+/* TODO
+ * - download finished svg
+ */
