67a04e6eea4595215c70612f74f77dceb574d8e9
Tarek Khelif <tokhelif@gmail.com>
2018-02-04 18:24:28 -0800

    Plan a better organized version of the example version of the app

---
+++ app_exampleVersion.js
@@ -0,0 +1,29 @@
+// import d3 from "D3";
+// import paper from "paper";
+/* global d3: false, paper: false */
+
+// Set up app as it would be before user has put in any data
+
+// Mock loading raster
+
+// Mock outlining raster
+
+// Mock generating sites for Voronoi tesselation
+
+/* TODO
+ * - move/add/delete sites
+ */
+
+// Mock coloring polygons based on raster
+
+/* TODO
+ * - move/add/delete vertices
+ * - add/delete links
+ * - default to recalculating color
+ * - optionally lock color so it isn't recalculated
+ * - choose custom color
+ */
+
+/* TODO
+ * - download finished svg
+ */
