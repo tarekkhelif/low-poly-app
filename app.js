83a7bb5177d04a5053f40e1a3e73b233b7531d63
Tarek Khelif <tokhelif@gmail.com>
2018-02-07 21:10:29 -0800

    Make whole project a class and each phase a method

--- app.js
+++ app.js
@@ -9,47 +9,93 @@
 // import d3 from "./node_modules/d3/dist/d3.js";
 // import paper from "./node_modules/paper/dist/paper-core.js";
 
-import { setUpWorkspace } from "./example/setUpWorkspace.js";
-import { getRaster } from "./example/getRaster.js";
-import { outlineRaster } from "./example/outlineRaster.js";
-import { generateSites } from "./example/generateSites.js";
-import { generateVoronoi } from "./example/generateVoronoi.js";
+import { setUpWorkspace as $setUpWorkspace } from "./example/setUpWorkspace.js";
+import { getRaster as $getRaster } from "./example/getRaster.js";
+import { outlineRaster as $outlineRaster } from "./example/outlineRaster.js";
+import { generateSites as $generateSites } from "./example/generateSites.js";
+import { generateVoronoi as $generateVoronoi } from "./example/generateVoronoi.js";
 
-app();
-async function app() {
-    // Initialize containers for usefull information, categorized by whether
-    //   they're the generic data that defines the project, or specific pieces
-    //   of either the D3 or Paper.js rendering of the project
-    const d3Project = {};
-    const pjsProject = {};
-    const data = {};
+class LowPolyProject {
+    constructor() {
+        // Initialize containers for usefull information, categorized by whether
+        //   they're the generic data that defines the project, or specific
+        //   pieces of either the D3 or Paper.js rendering of the project.
+        this.d3Project = {};
+        this.pjsProject = {};
+        this.data = {};
 
-    // Define example data
-    const exampleData = {
-        rasterPath: "../nile.jpg",
-        outlineFilePath: "../nw-outline.svg_outline_2018.02.02-23.21.10.csv",
-        sitesFilePath:
-            "../nw-outline.svg_points-inside_100_2018.02.02-23.21.38.csv",
-        width: 906.54926,
-        height: 604.36615
-    };
+        // Define example data
+        this.exampleData = {
+            rasterPath: "../nile.jpg",
+            outlineFilePath:
+                "../nw-outline.svg_outline_2018.02.02-23.21.10.csv",
+            sitesFilePath:
+                "../nw-outline.svg_points-inside_100_2018.02.02-23.21.38.csv",
+            width: 906.54926,
+            height: 604.36615
+        };
+    }
+
+    async run() {
+        this.setUpWorkspace();
+        await this.getRaster();
+        await this.outlineRaster();
+        this.generateSites(100);
+        this.generateVoronoi();
+    }
 
     // Set up blank workspace
-    setUpWorkspace(d3Project, pjsProject, data, exampleData);
+    setUpWorkspace() {
+        $setUpWorkspace(
+            this.d3Project,
+            this.pjsProject,
+            this.data,
+            this.exampleData
+        );
+    }
 
     // UI: Get raster
-    await getRaster(d3Project, pjsProject, data, exampleData);
+    async getRaster() {
+        await $getRaster(
+            this.d3Project,
+            this.pjsProject,
+            this.data,
+            this.exampleData
+        );
+    }
 
     // UI: Outline raster
-    await outlineRaster(d3Project, pjsProject, data, exampleData);
+    async outlineRaster() {
+        await $outlineRaster(
+            this.d3Project,
+            this.pjsProject,
+            this.data,
+            this.exampleData
+        );
+    }
 
     // Generate sites for initial Voronoi tesselation
-    generateSites(d3Project, pjsProject, data, exampleData, 100);
+    generateSites(n) {
+        $generateSites(
+            this.d3Project,
+            this.pjsProject,
+            this.data,
+            this.exampleData,
+            n
+        );
+    }
 
     // UI: Move/add/delete sites
 
     // Perform Voronoi tesselation and color based on raster
-    generateVoronoi(d3Project, pjsProject, data, exampleData);
+    generateVoronoi() {
+        $generateVoronoi(
+            this.d3Project,
+            this.pjsProject,
+            this.data,
+            this.exampleData
+        );
+    }
 
     // UI: Edit tesselation
     /* TODO
@@ -65,3 +111,6 @@ async function app() {
      * - download finished svg
     */
 }
+
+const project = new LowPolyProject();
+project.run();
