6fc99d9c0b962011c258d3574352baf23daf1bb0
Tarek Khelif <tokhelif@gmail.com>
2018-02-19 01:36:16 -0800

    Get Flow working (and eslint & prettier)

---
+++ src/index.jsx
@@ -0,0 +1,46 @@
+/* @flow */
+/* eslint-disable no-console */
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css";
+
+if (!document.body) {
+    throw new Error("This documeny has no body 👀");
+}
+
+const body: HTMLElement = document.body;
+
+const myDiv = document.createElement("div");
+body.appendChild(myDiv);
+
+ReactDOM.render(<h1>AM I USING REACT NOW?</h1>, myDiv);
+
+// module.hot.accept();
+
+const emojis = document.createElement("p");
+emojis.innerText = "👀";
+myDiv.appendChild(emojis);
+
+function flowwwy(a: string, b: number) {
+    return a + b;
+}
+
+flowwwy("asdf", "sdffs");
+
+const arr = [...Array(20)];
+
+const p = document.createElement("p");
+
+let ptext = "";
+arr.forEach(() => {
+    ptext += `${Math.floor(Math.random() * 20)} `;
+});
+p.innerText = ptext;
+
+body.appendChild(p);
+
+const prom = new Promise((res) => console.log("promiseeeee", res));
+console.log(prom);
+// throw new Error("errorrororororor");
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+/* @flow */
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
@@ -6,38 +8,41 @@ const webpack = require("webpack");
 const distDir = "./dist";
 
 module.exports = {
-  entry: { reactHotLoader: "react-hot-loader/patch", main: "./src/index.js" },
-  plugins: [
-    new CleanWebpackPlugin([distDir]),
-    new HtmlWebpackPlugin({ title: "Monsterrr" }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, distDir),
-    publicPath: "/"
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    contentBase: distDir,
-    port: 3003,
-    hot: true
-  }
+    entry: {
+        reactHotLoader: "react-hot-loader/patch",
+        main: "./src/index.jsx"
+    },
+    plugins: [
+        new CleanWebpackPlugin([distDir]),
+        new HtmlWebpackPlugin({ title: "Monsterrr" }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, distDir),
+        publicPath: "/"
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        contentBase: distDir,
+        port: 3003,
+        hot: true
+    }
 };
