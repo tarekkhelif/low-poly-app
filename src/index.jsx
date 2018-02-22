a055c5d11b96e25a641e0def999434406c867d32
Tarek Khelif <tokhelif@gmail.com>
2018-02-21 18:21:15 -0800

    Clean up index.jsx and style.scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 /* @flow */
-/* eslint-disable no-console */
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -15,45 +14,4 @@ const body: HTMLElement = document.body;
 const myDiv = document.createElement("div");
 body.appendChild(myDiv);
 
-ReactDOM.render(<h1>AM I USING REACT NOW?</h1>, myDiv);
-
-// module.hot.accept();
-
-const emojis = document.createElement("p");
-emojis.innerText = "👀";
-myDiv.appendChild(emojis);
-
-function flowwwy(a: string, b: number) {
-    return a + b;
-}
-
-// $FlowFixMe
-flowwwy("asdf", "sdffs");
-
-const arr = [...Array(20)];
-
-const p = document.createElement("p");
-
-let ptext = "";
-arr.forEach(() => {
-    ptext += `${Math.floor(Math.random() * 20)} `;
-});
-p.innerText = ptext;
-
-body.appendChild(p);
-
-console.log("many Kinds of cheese");
-
-const prom = new Promise((res) => console.log("promiseeeee", res));
-console.log(prom);
-
-class LookAClass {
-    constructor(lemonade: string, bathTowel: Array<number>) {
-        console.log(lemonade, bathTowel);
-    }
-}
-
-const aClass = new LookAClass("banana flavored", [4, 3, 25, 4, 3]);
-console.log(aClass);
-
-// throw new Error("errorrororororor");
+ReactDOM.render(<h1>Hello World!</h1>, myDiv);
