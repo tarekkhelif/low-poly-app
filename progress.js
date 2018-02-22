8f6f6a056c7dc081eb5fa4a69969a0ba6ce4ebf7
Tarek Khelif <tokhelif@gmail.com>
2018-02-21 19:50:43 -0800

    Commit left over stuff from when I was working on this a long time ago

---
+++ progress.js
@@ -0,0 +1,170 @@
+/*
+    TODO: Validate `.csv` data before plotting/displaying
+          - Currently I'm assuming all data is in the positive quadrant, ie
+            I'm displaying x: (0, x_max)
+                           y: (0, y_max)
+*/
+// eslint-disable-next-line no-console
+console.log("starting draw-path.js");
+
+var numberOfDataSets = 0;
+var sites;
+var bbox;
+var voronoi = new Voronoi();
+
+function onDocumentDrag(event) {
+    event.preventDefault();
+}
+
+function onDocumentDrop(event) {
+    event.preventDefault();
+
+    var f = event.dataTransfer.files[0];
+
+    // Get reference
+    var table = document.getElementById("table");
+
+    // PapaParse
+    Papa.parse(f, {
+        dynamicTyping: true, // Recognize as numbers
+        skipEmptyLines: true, // My `.csv` files end in an empty line, which screws some things up
+
+        error: function (err, file) {
+            // eslint-disable-next-line no-console
+            console.log("ERROR:", err, file);
+        },
+
+        complete: function (results) {
+            numberOfDataSets += 1;
+
+            // Turn data into array of paperjs `Points`
+            var points = [];
+            results.data.forEach(function (row) {
+                points.push(new Point(row));
+            });
+
+            // Find x_max and y_max
+            function slice2D(col, arr) {
+                return arr.map(function (row) {
+                    return row[col];
+                });
+            }
+            var xs = slice2D(0, results.data);
+            var ys = slice2D(1, results.data);
+            var xMax = Math.max.apply(null, xs);
+            var yMax = Math.max.apply(null, ys);
+
+            // Resize canvas to fit data
+            view.viewSize.width = Math.max(xMax, view.viewSize.width);
+            view.viewSize.height = Math.max(yMax, view.viewSize.height);
+            view.center = new Point(view.size.width / 2, view.size.height / 2);
+
+            bbox = {
+                xl: 0,
+                xr: view.bounds.width,
+                yt: 0,
+                yb: view.bounds.height
+            };
+
+            // Plot first data set as path
+            if (numberOfDataSets !== 1) {
+                var outline = new Path({
+                    segments: points,
+                    // strokeColor: 'black',
+                    // fillColor: 'blue',
+                    strokeWidth: 3
+                });
+                var box = Path.Rectangle(outline.bounds);
+                // eslint-disable-next-line no-console
+                console.log(box);
+                var cover = box.subtract(outline);
+                cover.fillColor = "white";
+            } else {
+                sites = points;
+                renderDiagram();
+            }
+
+            points.forEach(function (point) {
+                // Display data as table
+                var tr = document.createElement("tr");
+                var tdx = document.createElement("td");
+                var tdy = document.createElement("td");
+                tdx.innerText = Math.round(point.x);
+                tdy.innerText = Math.round(point.y);
+                tr.appendChild(tdx);
+                tr.appendChild(tdy);
+                table.appendChild(tr);
+
+                /* // Plot data in canvas
+                new Path.Circle({
+                    center: point,
+                    radius: 4,
+                    fillColor: (numberOfDataSets === 1) ? '#009900': '#FF0000',
+                    opacity: 1
+                }); */
+            });
+
+            // Actually update the canvas
+            view.update();
+        }
+    });
+}
+
+function renderDiagram() {
+    var diagram = voronoi.compute(sites, bbox);
+    if (diagram) {
+        for (var i = 0, l = sites.length; i < l; i++) {
+            var cell = diagram.cells[sites[i].voronoiId];
+            if (cell) {
+                var halfedges = cell.halfedges;
+                var length = halfedges.length;
+                if (length > 2) {
+                    var points = [];
+                    for (var j = 0; j < length; j++) {
+                        var v = halfedges[j].getEndpoint();
+                        points.push(new Point(v));
+                    }
+                    createPath(points, sites[i]);
+                }
+            }
+        }
+    }
+}
+
+function createPath(points) {
+    var path = new Path();
+    path.fillColor = "#eac086";
+    // path.strokeColor = 'black';
+    path.closed = true;
+
+    for (var i = 0, l = points.length; i < l; i++) {
+        var point = points[i];
+        var next = points[i + 1 === points.length ? 0 : i + 1];
+        var vector = (next - point) / 2;
+        path.add({
+            point: point + vector,
+            handleIn: -vector,
+            handleOut: vector
+        });
+    }
+    path.scale(0.95);
+    removeSmallBits(path);
+    return path;
+}
+
+function removeSmallBits(path) {
+    var min = path.length / 50;
+    for (var i = path.segments.length - 1; i >= 0; i--) {
+        var segment = path.segments[i];
+        var cur = segment.point;
+        var nextSegment = segment.next;
+        var next = nextSegment.point + nextSegment.handleIn;
+        if (cur.getDistance(next) < min) {
+            segment.remove();
+        }
+    }
+}
+
+document.addEventListener("drop", onDocumentDrop, false);
+document.addEventListener("dragover", onDocumentDrag, false);
+document.addEventListener("dragleave", onDocumentDrag, false);
---
+++ roadmap.md
@@ -0,0 +1,8 @@
+# Roadmap
+
+* Draw outline
+* Initialize network with random Voronoi, and render
+* Modify Voronoi
+* Lock Voronoi (not constrained to be Voronoi)
+* Modify network (and colors?)
+* Export to SVG
