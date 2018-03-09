414e8f602f8ac3ecabbefac90f2d35dd97acf488
Tarek Khelif <tokhelif@gmail.com>
2018-03-08 20:56:01 -0800

    Scale raster to reasonable size

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -36,8 +36,28 @@ const App = ({ rasterBase64, width, height }) =>
             </svg>
         </div>);
 
+function scaleDimensions(fullWidth, fullHeight) {
+    if (fullWidth === 0 || fullHeight === 0) {
+        throw new Error("Dimensions must be nonzero");
+    }
+
+    const width = 600;
+    const height = fullHeight / fullWidth * width;
+
+    return { width, height };
+}
+
 const mapStateToProps = (state) => {
-    const { rasterBase64, width, height } = state.raster;
-    return { rasterBase64, width, height };
+    const {
+        rasterBase64,
+        width: nativeWidth,
+        height: nativeHeight
+    } = state.raster;
+
+    if (nativeHeight !== 0 && nativeHeight !== 0) {
+        const { width, height } = scaleDimensions(nativeWidth, nativeHeight);
+        return { rasterBase64, width, height };
+    }
+    return { rasterBase64, width: nativeWidth, height: nativeHeight };
 };
 export const ConnectedApp = connect(mapStateToProps)(App);
