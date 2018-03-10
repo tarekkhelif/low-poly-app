334fa32b0a7d5856635e54c15f691842298a9dc0
Tarek Khelif <tokhelif@gmail.com>
2018-03-09 22:39:42 -0800

    Select patch on click

--- src/app/components/DefaultToolUI.jsx
+++ src/app/components/DefaultToolUI.jsx
@@ -6,20 +6,34 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import { setSelectionAction } from "../actions/actionGenerators";
+
 import { Outline } from "./Outline";
 import { Mesh } from "./Mesh";
 
 const mapStateToProps = ({ patches }) => ({ patches });
-export const DefaultToolUI = connect(mapStateToProps)(({ patches }) => (
-    <g className="defaultToolUI">
-        {Object.entries(patches).map((patchEntry) => {
-            const [patchId, { outline, mesh }] = patchEntry;
-            return (
-                <g key={patchId} id={patchId} className="patch">
-                    <Outline id={`${patchId}-outline`} outline={outline} />
-                    <Mesh id={`${patchId}-mesh`} mesh={mesh} />
-                </g>
-            );
-        })}
-    </g>
-));
+export const DefaultToolUI = connect(mapStateToProps)((props) => {
+    const { dispatch, patches } = props;
+
+    return (
+        <g className="defaultToolUI">
+            {Object.entries(patches).map((patchEntry) => {
+                const [patchId, { outline, mesh }] = patchEntry;
+                return (
+                    <g
+                        key={patchId}
+                        id={patchId}
+                        className="patch"
+                        onMouseDown={(e) => {
+                            dispatch(setSelectionAction(patchId));
+                            e.stopPropagation();
+                        }}
+                    >
+                        <Outline id={`${patchId}-outline`} outline={outline} />
+                        <Mesh id={`${patchId}-mesh`} mesh={mesh} />
+                    </g>
+                );
+            })}
+        </g>
+    );
+});
