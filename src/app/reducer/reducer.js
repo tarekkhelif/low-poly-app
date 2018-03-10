f90986b176cb0c122dcca4b04948aa5462e865e6
Tarek Khelif <tokhelif@gmail.com>
2018-03-09 22:25:41 -0800

    Handle SET_SELECTION in reducer. Log state on each action

--- src/app/reducer/reducer.js
+++ src/app/reducer/reducer.js
@@ -1,34 +1,32 @@
-import {
-    SET_RASTER,
-    CHANGE_TOOL,
-    OUTLINE_TOOL,
-} from "../actions/actionTypes";
+import { SET_SELECTION, SET_RASTER, CHANGE_TOOL } from "../actions/actionTypes";
 
 import testState from "./testState.json";
 
-const defaultState = {
-    raster: {
-        rasterBase64: "",
-        width: 600,
-        height: 400
-    },
-    currentTool: {
-        tool: OUTLINE_TOOL
-    }
-};
-
 export const reducer = (state = testState, action) => {
-    console.log(action);
-    if (action.type === SET_RASTER) {
-        const nextState = { ...state };
-        const { rasterBase64, width, height } = action.payload;
-        nextState.raster = { rasterBase64, width, height };
-        return nextState;
-    } else if (action.type === CHANGE_TOOL) {
-        const nextState = { ...state };
-        const { tool } = action.payload;
-        nextState.currentTool = { tool };
-        return nextState;
+    console.log("action: ", action);
+    let nextState;
+    switch (action.type) {
+        case SET_SELECTION: {
+            nextState = { ...state };
+            const { id } = action.payload;
+            nextState.selection = id;
+            break;
+        }
+        case SET_RASTER: {
+            nextState = { ...state };
+            const { rasterBase64, width, height } = action.payload;
+            nextState.raster = { rasterBase64, width, height };
+            break;
+        }
+        case CHANGE_TOOL: {
+            nextState = { ...state };
+            const { tool } = action.payload;
+            nextState.currentTool = { tool };
+            break;
+        }
+        default:
+            nextState = state;
     }
-    return state
+    console.log("state: ", nextState);
+    return nextState;
 };
