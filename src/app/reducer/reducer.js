e19a8692295d4a07f9b6a6b2e2f16fe6c8c757a0
Tarek Khelif <tokhelif@gmail.com>
2018-03-10 16:23:28 -0800

    Actually deep copy state in the reducer
    Before I was using `{ ...state }` to get a copy of state to modify to
    make the next state.  But that makes a shallow copy.  Now I'm using
    `JSON.parse(JSON.stringify(state))`.
    
    Shallow copying caused some hard-to-track-down errors.  When I tried to
    delete a node from the outline, it looked like it was deleted from
    state, but it was still displayed on screen.  Turns out I'd mutated the
    deeply nested `state.patches[patchId].outline.nodes[nodeId]` object and
    copied the reference to `state.patches` to `nextState`, so since
    `state.patches` and `nextState.patches` were both pointing to the same
    object, react-redux (I think) didn't think `patches` had changed and so
    didn't rerender the components that depend on `patches`.
    
    If I'd used Immutable.js would I would have avoided this problem.

--- src/app/reducer/reducer.js
+++ src/app/reducer/reducer.js
@@ -1,27 +1,48 @@
-import { SET_SELECTION, SET_RASTER, CHANGE_TOOL } from "../actions/actionTypes";
+import {
+    SET_SELECTION,
+    SET_RASTER,
+    CHANGE_TOOL,
+    DELETE_OUTLINE_NODE,
+    MOVE_OUTLINE_NODE
+} from "../actions/actionTypes";
 
 import testState from "./testState.json";
+
+// SO says this is a fast way to get a deep copy
+const deepCopy = (obj) => JSON.parse(JSON.stringify(obj));
 
 export const reducer = (state = testState, action) => {
     console.log("action: ", action);
     let nextState;
     switch (action.type) {
         case SET_SELECTION: {
-            nextState = { ...state };
+            nextState = deepCopy(state);
             const { id } = action.payload;
             nextState.selection = id;
             break;
         }
         case SET_RASTER: {
-            nextState = { ...state };
+            nextState = deepCopy(state);
             const { rasterBase64, width, height } = action.payload;
             nextState.raster = { rasterBase64, width, height };
             break;
         }
         case CHANGE_TOOL: {
-            nextState = { ...state };
+            nextState = deepCopy(state);
             const { tool } = action.payload;
             nextState.currentTool = { tool };
+            break;
+        }
+        case DELETE_OUTLINE_NODE: {
+            nextState = deepCopy(state);
+            const { patchId, nodeId } = action.payload;
+            delete nextState.patches[patchId].outline.nodes[nodeId];
+            break;
+        }
+        case MOVE_OUTLINE_NODE: {
+            nextState = deepCopy(state);
+            const { patchId, nodeId, newPoint } = action.payload;
+            nextState.patches[patchId].outline.nodes[nodeId].point = newPoint;
             break;
         }
         default:
