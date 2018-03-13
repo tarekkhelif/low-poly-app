fdb9a60e69a4f5be9f80d0b72f032089b206a4f0
Tarek Khelif <tokhelif@gmail.com>
2018-03-12 20:42:30 -0700

    Simplify OutlineToolPatch to a functional component

--- src/app/components/OutlineToolPatch.jsx
+++ src/app/components/OutlineToolPatch.jsx
@@ -19,67 +19,22 @@ type Props = {
     patchEventHandler: (e: Event) => void
 };
 
-export const OutlineToolPatch = class extends React.Component<Props> {
-    componentDidMount() {
-        this.installListeners();
-    }
-
-    componentWillUpdate() {
-        this.uninstallListeners();
-    }
-
-    componentDidUpdate() {
-        this.installListeners();
-    }
-
-    componentWillUnmount() {
-        this.uninstallListeners();
-    }
-
-    installListeners() {
-        const { patchElement } = this;
-        const { patchEventHandler } = this.props;
-
-        // Handlers to un/install in `componentDidMout` / `componentWillUnmount`
-        this.handlers = [
-            {
-                // This patch
-                element: patchElement,
-                event: "mousedown.outlineTool.patch",
-                handler: () =>
-                    executeOnPlainMouseDown(patchEventHandler)(d3.event)
-            }
-        ];
-
-        // Install listeners
-        this.handlers.forEach(({ element, event, handler }) =>
-            d3.select(element).on(event, handler));
-    }
-
-    uninstallListeners() {
-        // Uninstall listeners
-        this.handlers.forEach(({ element, event }) =>
-            d3.select(element).on(event, null));
-    }
-
-    render() {
-        const { patchId, outline, selected } = this.props;
-
-        return (
-            <g
-                id={patchId}
-                className="patch"
-                ref={(patchElement) => {
-                    this.patchElement = patchElement;
-                }}
-            >
-                <Outline
-                    id={`${patchId}-outline`}
-                    selected={selected}
-                    outline={outline}
-                />
-                <OutlineNodes patchId={patchId} selected={selected} />
-            </g>
-        );
-    }
-};
+export const OutlineToolPatch = ({
+    patchId,
+    outline,
+    selected,
+    patchEventHandler
+}: Props) => (
+    <g
+        id={patchId}
+        className="patch"
+        onMouseDown={executeOnPlainMouseDown(patchEventHandler)}
+    >
+        <Outline
+            id={`${patchId}-outline`}
+            selected={selected}
+            outline={outline}
+        />
+        <OutlineNodes patchId={patchId} selected={selected} />
+    </g>
+);
